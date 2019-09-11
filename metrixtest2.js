a3fa930a07d90117d8193f4ba7f4068dc8f22883
Wildfireone <wildfireone@gmail.com>
2019-09-11 21:41:07 +0100

    inital setup

---
+++ metrixtest2.js
@@ -0,0 +1,22 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  // get a reference to scrollcontroller
+const scrollController = require('fivesquared')
+
+// make an array of 0-255 pixels
+let arr = []
+
+for(var i = 0; i < 119; i++){
+    arr[i] = i%2 == 0 ? 255 : 0
+}
+
+// scrollController will drop messages it receives before the display is init'ed
+// so we wait 100ms and then call display with our pixel array.
+setTimeout(()=>{
+    scrollController.display(arr)
+},100)
+
+
+});
